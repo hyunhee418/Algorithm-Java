435d7d76f8209b84065ae5550c3e3064b34222d3
hyunhee418 <hyunhee18@gmail.com>
2024-01-03 00:27:00 +0900

    first commit for Algorithm

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        nums1 = mergeSortedArray.solution(nums1, m, nums2, n);
+        for (int i: nums1) {
+            System.out.print(i);
+        }
+    }
+}
